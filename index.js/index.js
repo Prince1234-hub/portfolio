551ac32d6e62cc52a48e988b6103283f421aaea0
Prince1234-hub <princealsoking445@gmail.com>
2025-03-01 19:54:01 +0530

    first commit

---
+++ index.js/index.js
@@ -0,0 +1,42 @@
+// let apple = prompt("enter your name")
+// let mango = prompt("enter you second ")
+// let operator = prompt("enter your operator")
+
+// let convert = Number(apple)
+// let second = Number(mango)
+
+// if (operator === "+") {
+//     let sum = convert + second
+//     console.log(sum)
+// }
+// else if(operator === "-") {
+//     let sub = convert - second
+//     console.log(sub)
+
+// }
+
+// else if(operator === "*") {
+//     let multiple = convert * second
+//     console.log(multiple)
+// }
+
+
+
+// //let marks = 500;
+// let marksinnumber = Number(marks)
+
+// console.log(marksinnumber)
+
+// let marks = 500
+// let markstostring = markstostring()
+
+ let number = 9506928274
+let numtostring = number.toString();
+
+if (numtostring.length == 10) {
+    console.log("correct number")
+    
+} else {
+    console.log("enter a valid number")
+    
+}
